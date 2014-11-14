f5b9f45b18cbcde020d5cb2e81d808fb66c058b2
Squatid <daniel.michalke@gmx.de>
2014-11-14 12:41:07 +0100

    doneSet and doneSetTest implemented

--- src/main/java/uni/ca/crawlingsim/crawler/DoneSet.java
+++ src/main/java/uni/ca/crawlingsim/crawler/DoneSet.java
@@ -1,30 +1,50 @@
 package uni.ca.crawlingsim.crawler;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import uni.ca.crawlingsim.data.Data;
 
 public class DoneSet {
 	private Data data;
-	
+	private static String tableName = "DoneSetTable";
 	public DoneSet() throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException{
 		this.data = new Data();
 		this.createTable();
 	}
 
-	private void createTable() {
+	private void createTable() throws SQLException {
 		// TODO Auto-generated method stub
-		
+		if (data.containsTable(tableName)){
+			Statement statement = data.createStatement();
+			statement.execute("drop table "+tableName);
+			statement.close();
+		}
+		Statement statement = data.createStatement();
+		statement.execute("create table "+tableName+" (url varchar(32))");
+		statement.close();
 	}
 
-	public void add(String url) {
+	public void add(String url) throws SQLException {
+		PreparedStatement preparedStatement = data.prepareStatement("INSERT INTO "+tableName+" VALUES (?)");
+		preparedStatement.setString(1, url);
+		preparedStatement.execute();
+		preparedStatement.close();
 		// TODO Auto-generated method stub
-		
 	}
 
-	public boolean contains(String urlElement) {
+	public boolean contains(String url) throws SQLException {
 		// TODO Auto-generated method stub
-		return false;
+		PreparedStatement preparedStatement = data.prepareStatement("SELECT * FROM "+tableName+" WHERE url = ?");
+		preparedStatement.setString(1, url);
+		preparedStatement.execute();
+		ResultSet resultSet = preparedStatement.getResultSet();
+		boolean result = resultSet.next();
+		resultSet.close();
+		preparedStatement.close();
+		return result;
 	}
 
 	public void close() throws SQLException {
---
+++ src/test/java/uni/ca/crawlingsim/tests/DoneSetTest.java
@@ -0,0 +1,23 @@
+package uni.ca.crawlingsim.tests;
+
+import static org.junit.Assert.*;
+
+import java.sql.SQLException;
+
+import org.junit.Test;
+
+import uni.ca.crawlingsim.crawler.DoneSet;
+
+public class DoneSetTest {
+
+	@Test
+	public void test() throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
+		DoneSet done = new DoneSet();
+		done.add("a");
+		done.add("b");
+		assertTrue(done.contains("a"));
+		assertTrue(done.contains("b"));
+		assertTrue(!done.contains("c"));
+	}
+
+}
